a44c9e8961f6af78c9a7c0ad21bacf8b2eb951e8
agumusay <agumusay@protonmail.com>
2020-07-17 23:29:16 +0200

    some more style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 import productsData from './data/products.json';
 import Home from './pages/home';
 import Products from './pages/products';
@@ -7,29 +8,25 @@ import ProductDetails from './pages/productDetails';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div className="logo"></div>
-
-        <Switch>
-          <Route
-            path="/products/:slug"
-            render={(props) => <ProductDetails {...props} productsData={productsData} />}
-          />
-          <Route
-            path="/products"
-            render={(props) => <Products {...props} productsData={productsData} />}
-          />
-          <Route
-            exact
-            path="/"
-            render={(props) => <Home {...props} productsData={productsData} />}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Link to='/'>
+					<div className='logo-container'>
+						<div className='logo'></div>
+						<span>Awesome Company</span>
+					</div>
+				</Link>
+				<AnimatePresence exitBeforeEnter>
+					<Switch>
+						<Route path='/products/:slug' render={props => <ProductDetails {...props} productsData={productsData} />} />
+						<Route path='/products' render={props => <Products {...props} productsData={productsData} />} />
+						<Route exact path='/' render={props => <Home {...props} productsData={productsData} />} />
+					</Switch>
+				</AnimatePresence>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Home = (props) => {
-  const topProducts = [...props.productsData].sort((a, b) => b.price - a.price).slice(0, 4);
-  return (
-    <div className="home">
-      <div className="home-header">
-        <h2 className="home-header-title">Welcome, visitor!</h2>
-        <NavLink to="/products">
-          <button className="home-header-nav">Go To Products</button>
-        </NavLink>
-      </div>
+const Home = props => {
+	const topProducts = [...props.productsData].sort((a, b) => b.price - a.price).slice(0, 4);
+	return (
+		<div className='home'>
+			<div className='home-header'>
+				<h2 className='home-header-title'>Welcome, visitor!</h2>
+				<NavLink to='/products'>
+					<button className='home-header-nav'>Go To Products</button>
+				</NavLink>
+			</div>
 
-      <ul className="top-products">
-        {topProducts.map(({ id, name, price, slug }) => {
-          return (
-            <NavLink to={`/products/${slug}`} key={id}>
-              <li className="top">
-                <h1 className="top-title">{name}</h1>
-                <div className="top-price">{`$${price}`}</div>
-              </li>
-            </NavLink>
-          );
-        })}
-      </ul>
-    </div>
-  );
+			<ul className='top-products'>
+				{topProducts.map(({ id, name, price, slug, shortDescription, image }) => {
+					return (
+						<NavLink to={`/products/${slug}`} key={id}>
+							<li className='top'>
+								<img src={image} className='top-left'></img>
+								<div className='top-right'>
+									<h1 className='top-title'>{name}</h1>
+									<p className='top-description'>{shortDescription}</p>
+								</div>
+
+								<div className='top-price'>{`$${price}`}</div>
+							</li>
+						</NavLink>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default Home;
